fca6b82368f7868e8cc214506cc042fae8d6a438
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-06 13:14:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,7 +73,7 @@ def current_player
     end
 end 
 
-def won?(board)
+def won?
   WIN_COMBINATIONS.find do |win_combo|
   @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]] && position_taken?(@board, win_combo[0])
   end
